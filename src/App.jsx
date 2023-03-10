7b7ddcc6bef5ed73ccc6a12f5e5f58245e5b091a
reddenial <danielgdrojas@gmail.com>
2023-03-10 13:22:45 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { ExpenseInput } from "components/ExpenseInput/ExpenseInput";
+import { ExpenseTotal } from "components/ExpenseTotal/ExpenseTotal";
+import { IncomeInput } from "components/IncomeInput/IncomeInput";
+import { List } from "components/List/List";
+import { Logo } from "components/Logo/Logo";
+import { useSelector } from "react-redux";
+import s from "./style.module.css";
+
+export function App() {
+  const expenseList = useSelector(store => store.EXPENSE.expenseList);
+
+  return (
+    <div className={s.main_container}>
+      <div className={`row ${s.header}`}>
+        <div className={`col-3`}>
+          <Logo title="iSpent" subtitle="expense tracker" />
+        </div>
+        <div className={`col-9 ${s.income_input}`}>
+          <IncomeInput />
+        </div>
+      </div>
+      <div className={`row ${s.workspace}`}>
+        <div className={`col-12  ${s.expense_input}`}>
+          <ExpenseInput />
+        </div>
+        <div className={`col-11 col-md-6 col-lg-4 ${s.expense_list}`}>
+          <List 
+            items={expenseList}
+          />
+          <div className={`col-12 ${s.expense_total}`}>
+            <ExpenseTotal />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
